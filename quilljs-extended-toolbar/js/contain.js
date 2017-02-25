6f279e42332f810245476b23061dd733d97a5af2
dost <tomas@timepress.cz>
2017-02-25 01:21:31 +0100

    table cells as containers

--- quilljs-extended-toolbar/js/contain.js
+++ quilljs-extended-toolbar/js/contain.js
@@ -7,8 +7,6 @@ let BlockEmbed = Quill.import('blots/block/embed');
 let TextBlot = Quill.import('blots/text');
 
 
-const NEWLINE_LENGTH = 1;
-
 //
 //
 // CONTAINER TAG
@@ -21,19 +19,55 @@ class ContainBlot extends Container {
     return node;
   }
 
-  static formats(domNode) {
-    return domNode.tagName;
+  insertBefore(blot, ref) {
+    if (blot.statics.blotName == this.statics.blotName) {
+      console.log('############################ Not sure this is clean:')
+      console.log(blot)
+      console.log(blot.children.head)
+      super.insertBefore(blot.children.head, ref);
+    } else {
+      super.insertBefore(blot, ref);
+    }
   }
 
-  format(name, value) {
-    if (this.children.length > 0) {
-      this.children.tail.format(name, value);
-    }
+  static formats(domNode) {
+    return domNode.tagName;
   }
 
   formats() {
     // We don't inherit from FormatBlot
     return { [this.statics.blotName]: this.statics.formats(this.domNode) }
+  }
+
+  replace(target) {
+    if (target.statics.blotName !== this.statics.blotName) {
+      let item = Parchment.create(this.statics.defaultChild);
+      target.moveChildren(item);
+      this.appendChild(item);
+    }
+    if (target.parent == null) return;
+    super.replace(target)
+  }
+}
+
+ContainBlot.blotName = 'contain';
+ContainBlot.tagName = 'contain';
+ContainBlot.scope = Parchment.Scope.BLOCK_BLOT;
+ContainBlot.defaultChild = 'block';
+ContainBlot.allowedChildren = [Block, BlockEmbed, Container];
+Quill.register(ContainBlot);
+
+
+//
+//
+// CONTAINER TR
+//
+
+class TableRow extends Container {
+  static create(value) {
+    let tagName = 'tr';
+    let node = super.create(tagName);
+    return node;
   }
 
   optimize() {
@@ -47,22 +81,105 @@ class ContainBlot extends Container {
     }
   }
 
-  replace(target) {
-    if (target.statics.blotName !== this.statics.blotName) {
-      let item = Parchment.create(this.statics.defaultChild);
-      target.moveChildren(item);
-      this.appendChild(item);
+}
+
+TableRow.blotName = 'tr';
+TableRow.tagName = 'tr';
+TableRow.scope = Parchment.Scope.BLOCK_BLOT;
+TableRow.defaultChild = 'td';
+Quill.register(TableRow);
+
+//
+//
+// CONTAINER TABLE
+//
+
+class Table extends Container {
+  static create(value) {
+    let tagName = 'table';
+    let node = super.create(tagName);
+    return node;
+  }
+
+  optimize() {
+    super.optimize();
+    let next = this.next;
+    if (next != null && next.prev === this &&
+        next.statics.blotName === this.statics.blotName &&
+        next.domNode.tagName === this.domNode.tagName) {
+      next.moveChildren(this);
+      next.remove();
     }
-    super.replace(target);
+  }
+
+}
+
+Table.blotName = 'table';
+Table.tagName = 'table';
+Table.scope = Parchment.Scope.BLOCK_BLOT;
+Table.defaultChild = 'tr';
+Table.allowedChildren = [TableRow];
+Quill.register(Table);
+
+//
+//
+// CONTAINER TD
+//
+
+class TableCell extends ContainBlot {
+  static create(value) {
+    let node = super.create('td');
+    node.setAttribute('id', value);
+    return node;
+  }
+
+  format() {
+    this.getAttribute('id');
+  }
+
+  formats() {
+    // We don't inherit from FormatBlot
+    return { [this.statics.blotName]: this.domNode.getAttribute('id') }
+  }
+
+  optimize() {
+    super.optimize();
+
+    // Add parent TR and TABLE when missing
+    let parent = this.parent;
+    console.log('optimizing parent for TD:')
+    console.log(parent)
+    if (parent != null && parent.statics.blotName != 'tr') {
+      let table = Parchment.create('table')
+      let tr = Parchment.create('tr')
+      table.appendChild(tr);
+      tr.appendChild(this);
+      parent.appendChild(table);
+    }
+
+    // merge same TD id
+    let next = this.next;
+    if (next != null && next.prev === this &&
+        next.statics.blotName === this.statics.blotName &&
+        next.domNode.tagName === this.domNode.tagName &&
+        next.domNode.getAttribute('id') === this.domNode.getAttribute('id')) {
+      next.moveChildren(this);
+      next.remove();
+    }
+    console.log("End of TD optimize")
   }
 }
 
-ContainBlot.blotName = 'contain';
-ContainBlot.tagName = 'contain';
-ContainBlot.scope = Parchment.Scope.BLOCK_BLOT;
-ContainBlot.defaultChild = 'block';
-ContainBlot.allowedChildren = [Block, BlockEmbed, Container];
+TableCell.blotName = 'td';
+TableCell.tagName = 'td';
+TableCell.scope = Parchment.Scope.BLOCK_BLOT;
+TableCell.defaultChild = 'block';
+TableCell.allowedChildren = [Block, BlockEmbed, Container];
+Quill.register(TableCell);
+TableRow.allowedChildren = [TableCell];
 
-Quill.register(ContainBlot);
+
+
+
+
 Quill.debug('info');
-
