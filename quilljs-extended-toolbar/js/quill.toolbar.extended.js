1df56ff981a5d37659c1d2d28fd7fadfaa7eaecd
Dave Nekovar <nekovar.david@gmail.com>
2017-03-07 09:54:00 +0100

    index.html > embed table, toolbar - make icons nice ;)

--- quilljs-extended-toolbar/js/quill.toolbar.extended.js
+++ quilljs-extended-toolbar/js/quill.toolbar.extended.js
@@ -13,127 +13,127 @@ let TextBlot = Quill.import('blots/text');
 // TABLE TAG
 //
 
-class tableCell extends Block {
-
-  static create (value) {
-    // console.log('create' + ' (' + this.blotName.toUpperCase() + ')');
-    // console.log('*** length() skipped' + ' (' + this.blotName.toUpperCase() + ') ***');
-    let node = super.create(value);
-    return node;
-  }
-
-  static formats(node) {
-    // console.log('formats' + ' (' + node.nodeName + ')');
-    // super.formats(node);
-  }
-
-  delta() {
-    let d = super.delta();
-    if (d && d['ops'] && d['ops'][1] && d['ops'][1]['attributes']) {
-      d['ops'][1]['attributes']["table-cell"] = {
-        "cellId": "table-cell-zxwscqtywssf",
-        "rowId": "table-row-oksghsnmwiwc",
-        "id": "table-id-tpbprwzxdlvn"
-      };
-    }
-    return d;
-  }
-
-  format(name, value) {
-    console.log('format' + ' (' + this.domNode.nodeName + ')');
-    super.format(name, value);
-  }
-
-  formatAt(index, length, name, value) {
-    console.log('formatAt' + ' (' + this.domNode.nodeName+ ')');
-    super.formatAt(index, length, name, value);
-  }
-
-  insertBefore(blot, ref) {
-    console.log('insertBefore' + ' (' + this.domNode.nodeName + ')');
-    super.insertBefore(blot, ref);
-  }
-
-  insertAt(index, value, def) {
-    console.log('insertAt' + ' (' + this.domNode.nodeName + ')');
-    super.insertAt(index, value, def);
-  }
-
-  optimize() {
-    // console.log('optimize' + ' (' + this.domNode.nodeName + ')');
-    // super.optimize();
-  }
-
-  replace(target) {
-    console.log('replace' + ' (' + this.domNode.nodeName + ')');
-    super.replace(target);
-  }
-
-  remove() {
-    console.log('remove' + ' (' + this.domNode.nodeName + ')');
-    super.remove();
-  }
-
-  appendChild(other) {
-    console.log('appendChild' + ' (' + this.domNode.nodeName + ')');
-    super.appendChild(other);
-  }
-
-  attach() {
-    console.log('attach' + ' (' + this.domNode.nodeName + ')');
-    super.attach();
-  }
-
-  deleteAt(index, length) {
-    console.log('deleteAt' + ' (' + this.domNode.nodeName + ')');
-    super.deleteAt(index, length);
-  }
-
-  detach() {
-    console.log('detach' + ' (' + this.domNode.nodeName + ')');
-    super.detach();
-  }
-
-  // length() {
-  //   console.log('length' + ' (' + this.domNode.nodeName + ')');
-  //   super.length();
-  // }
-
-  moveChildren(targetParent, refNode) {
-    console.log('moveChildren' + ' (' + this.domNode.nodeName + ')');
-    super.moveChildren(targetParent, refNode);
-  }
-
-  // path(index, inclusive) {
-  //   console.log('path' + ' (' + this.domNode.nodeName + ')');
-  //   super.path(index, inclusive);
-  // }
-
-  removeChild(child) {
-    console.log('removeChild' + ' (' + this.domNode.nodeName + ')');
-    super.removeChild(child);
-  }
-
-  split(index, force) {
-    console.log('split' + ' (' + this.domNode.nodeName + ')');
-    super.split(index, force);
-  }
-
-  unwrap() {
-    console.log('uwrap' + ' (' + this.domNode.nodeName + ')');
-    super.unwrap();
-  }
-
-  update(mutations) {
-    console.log('uwrap' + ' (' + this.domNode.nodeName + ')');
-    super.update(mutations);
-  }
-
-}
-
-tableCell.blotName = 'table-cell';
-tableCell.tagName = 'td';
-Quill.register(tableCell);
+// class tableCell extends Block {
+//
+//   static create (value) {
+//     // console.log('create' + ' (' + this.blotName.toUpperCase() + ')');
+//     // console.log('*** length() skipped' + ' (' + this.blotName.toUpperCase() + ') ***');
+//     let node = super.create(value);
+//     return node;
+//   }
+//
+//   static formats(node) {
+//     // console.log('formats' + ' (' + node.nodeName + ')');
+//     // super.formats(node);
+//   }
+//
+//   delta() {
+//     let d = super.delta();
+//     if (d && d['ops'] && d['ops'][1] && d['ops'][1]['attributes']) {
+//       d['ops'][1]['attributes']["table-cell"] = {
+//         "cellId": "table-cell-zxwscqtywssf",
+//         "rowId": "table-row-oksghsnmwiwc",
+//         "id": "table-id-tpbprwzxdlvn"
+//       };
+//     }
+//     return d;
+//   }
+//
+//   format(name, value) {
+//     console.log('format' + ' (' + this.domNode.nodeName + ')');
+//     super.format(name, value);
+//   }
+//
+//   formatAt(index, length, name, value) {
+//     console.log('formatAt' + ' (' + this.domNode.nodeName+ ')');
+//     super.formatAt(index, length, name, value);
+//   }
+//
+//   insertBefore(blot, ref) {
+//     console.log('insertBefore' + ' (' + this.domNode.nodeName + ')');
+//     super.insertBefore(blot, ref);
+//   }
+//
+//   insertAt(index, value, def) {
+//     console.log('insertAt' + ' (' + this.domNode.nodeName + ')');
+//     super.insertAt(index, value, def);
+//   }
+//
+//   optimize() {
+//     // console.log('optimize' + ' (' + this.domNode.nodeName + ')');
+//     // super.optimize();
+//   }
+//
+//   replace(target) {
+//     console.log('replace' + ' (' + this.domNode.nodeName + ')');
+//     super.replace(target);
+//   }
+//
+//   remove() {
+//     console.log('remove' + ' (' + this.domNode.nodeName + ')');
+//     super.remove();
+//   }
+//
+//   appendChild(other) {
+//     console.log('appendChild' + ' (' + this.domNode.nodeName + ')');
+//     super.appendChild(other);
+//   }
+//
+//   attach() {
+//     console.log('attach' + ' (' + this.domNode.nodeName + ')');
+//     super.attach();
+//   }
+//
+//   deleteAt(index, length) {
+//     console.log('deleteAt' + ' (' + this.domNode.nodeName + ')');
+//     super.deleteAt(index, length);
+//   }
+//
+//   detach() {
+//     console.log('detach' + ' (' + this.domNode.nodeName + ')');
+//     super.detach();
+//   }
+//
+//   // length() {
+//   //   console.log('length' + ' (' + this.domNode.nodeName + ')');
+//   //   super.length();
+//   // }
+//
+//   moveChildren(targetParent, refNode) {
+//     console.log('moveChildren' + ' (' + this.domNode.nodeName + ')');
+//     super.moveChildren(targetParent, refNode);
+//   }
+//
+//   // path(index, inclusive) {
+//   //   console.log('path' + ' (' + this.domNode.nodeName + ')');
+//   //   super.path(index, inclusive);
+//   // }
+//
+//   removeChild(child) {
+//     console.log('removeChild' + ' (' + this.domNode.nodeName + ')');
+//     super.removeChild(child);
+//   }
+//
+//   split(index, force) {
+//     console.log('split' + ' (' + this.domNode.nodeName + ')');
+//     super.split(index, force);
+//   }
+//
+//   unwrap() {
+//     console.log('uwrap' + ' (' + this.domNode.nodeName + ')');
+//     super.unwrap();
+//   }
+//
+//   update(mutations) {
+//     console.log('uwrap' + ' (' + this.domNode.nodeName + ')');
+//     super.update(mutations);
+//   }
+//
+// }
+//
+// tableCell.blotName = 'table-cell';
+// tableCell.tagName = 'td';
+// Quill.register(tableCell);
 
 
 // class extendedContainer extends Container {
@@ -378,138 +378,138 @@ Quill.register(tableCell);
 // Table.scope = Parchment.Scope.BLOCK_BLOT;
 // Quill.register(Table);
 
-// //
-// //
-// // EMBED TABLE TAG
-// //
 //
-// class StaticInline extends Inline {}
-// Quill.register(StaticInline);
 //
-// class InlineTd extends StaticInline {}
+// EMBED TABLE TAG
 //
-// InlineTd.blotName = 'inline_td';
-// InlineTd.tagName = 'td';
-// Quill.register(InlineTd);
-//
-// class InlineTr extends StaticInline {}
-//
-// InlineTr.blotName = 'inline_tr';
-// InlineTr.tagName = 'tr';
-// Quill.register(InlineTr);
-//
-// class InlineTbody extends StaticInline {}
-//
-// InlineTbody.blotName = 'inline_tbody';
-// InlineTbody.tagName = 'tbody';
-// Quill.register(InlineTbody);
-//
-// class EmbedTable extends BlockEmbed {
-//   static create(value) {
-//     let node = super.create(value);
-//
-//     if(value === true || value.includes('newtable_')) {
-//       node = this.generate_empty_table(value, node);
-//     } else if(typeof value === 'string' || value.includes('td')) {
-//       node.innerHTML = value;
-//     }
-//
-//     return node;
-//   }
-//
-//   static generate_empty_table(value, node) {
-//     let cols = 5;
-//     let rows = 5;
-//     if(typeof value === 'string' && value.includes('newtable_')) {
-//       value = value.split('_');
-//       rows = value[1];
-//       cols = value[2];
-//     } else {
-//       rows = prompt('Number of rows?', cols);
-//       cols = prompt('Number of cols?', cols);
-//     }
-//     value = [];
-//     for (let r = 0; r < rows; r++) {
-//       value[r] = [];
-//       for (let c = 0; c < cols; c++) {
-//         value[r].push("");
-//       }
-//     }
-//     var tbody = document.createElement('tbody');
-//     value.forEach(row => {
-//       var tr = document.createElement('tr');
-//       tbody.appendChild(tr);
-//       row.forEach(cell => {
-//         let td = document.createElement('td');
-//         td.innerText = cell;
-//         tr.appendChild(td);
-//       })
-//     })
-//     node.appendChild(tbody);
-//     return node;
-//   }
-//
-//   // static value(node) {
-//   //   console.log('table value');
-//   //   var ret = [];
-//   //   if(node.tagName === 'table') {
-//   //     let rows = node.rows;
-//   //     for (let i = 0; i < rows.length; i++) {
-//   //       ret[i] = [];
-//   //       for (let j = 0; j < rows[i].cells.length; j++) {
-//   //         ret[i].push(rows[i].cells[j].innerText);
-//   //       }
-//   //     }
-//   //   }
-//   //   return ret;
-//   // }
-//
-//   // static formats(node) {
-//   //   console.log('table formats');
-//   //   super.formats(node);
-//   // }
-//
-//   delta() {
-//     // console.log('table delta');
-//     let d = new Delta().insert({ embed_table: this.domNode.innerHTML });
-//     return d;
-//   }
-//
-//   // format(name, value) {
-//   //   console.log('table format');
-//   //   // super.format(name, value);
-//   // }
-//
-//   // formatAt(index, length, name, value) {
-//   //   console.log('table formatAt');
-//   //   // super.formatAt(index, length, name, value);
-//   // }
-//
-//   // insertAt(index, value, def) {
-//   //   console.log('table insertAt');
-//   //   super.insertAt(index, value, def);
-//   // }
-//
-//   // optimize() {
-//   //   console.log('table optimize');
-//   //   super.optimize();
-//   // }
-//
-//   // replace(target) {
-//   //   console.log('table replace');
-//   //   super.replace(target);
-//   // }
-//
-//   remove() {
-//     console.log('table remove');
-//     var remove_table = confirm('Are you sure you want to remove this table?');
-//     if (remove_table) super.remove();
-//   }
-// }
-//
-// EmbedTable.blotName = 'embed_table';
-// EmbedTable.tagName = 'table';
-// Quill.register(EmbedTable);
+
+class StaticInline extends Inline {}
+Quill.register(StaticInline);
+
+class InlineTd extends StaticInline {}
+
+InlineTd.blotName = 'inline_td';
+InlineTd.tagName = 'td';
+Quill.register(InlineTd);
+
+class InlineTr extends StaticInline {}
+
+InlineTr.blotName = 'inline_tr';
+InlineTr.tagName = 'tr';
+Quill.register(InlineTr);
+
+class InlineTbody extends StaticInline {}
+
+InlineTbody.blotName = 'inline_tbody';
+InlineTbody.tagName = 'tbody';
+Quill.register(InlineTbody);
+
+class EmbedTable extends BlockEmbed {
+  static create(value) {
+    let node = super.create(value);
+
+    if(value === true || value.includes('newtable_')) {
+      node = this.generate_empty_table(value, node);
+    } else if(typeof value === 'string' || value.includes('td')) {
+      node.innerHTML = value;
+    }
+
+    return node;
+  }
+
+  static generate_empty_table(value, node) {
+    let cols = 5;
+    let rows = 5;
+    if(typeof value === 'string' && value.includes('newtable_')) {
+      value = value.split('_');
+      rows = value[1];
+      cols = value[2];
+    } else {
+      rows = prompt('Number of rows?', cols);
+      cols = prompt('Number of cols?', cols);
+    }
+    value = [];
+    for (let r = 0; r < rows; r++) {
+      value[r] = [];
+      for (let c = 0; c < cols; c++) {
+        value[r].push("");
+      }
+    }
+    var tbody = document.createElement('tbody');
+    value.forEach(row => {
+      var tr = document.createElement('tr');
+      tbody.appendChild(tr);
+      row.forEach(cell => {
+        let td = document.createElement('td');
+        td.innerText = cell;
+        tr.appendChild(td);
+      })
+    })
+    node.appendChild(tbody);
+    return node;
+  }
+
+  // static value(node) {
+  //   console.log('table value');
+  //   var ret = [];
+  //   if(node.tagName === 'table') {
+  //     let rows = node.rows;
+  //     for (let i = 0; i < rows.length; i++) {
+  //       ret[i] = [];
+  //       for (let j = 0; j < rows[i].cells.length; j++) {
+  //         ret[i].push(rows[i].cells[j].innerText);
+  //       }
+  //     }
+  //   }
+  //   return ret;
+  // }
+
+  // static formats(node) {
+  //   console.log('table formats');
+  //   super.formats(node);
+  // }
+
+  delta() {
+    // console.log('table delta');
+    let d = new Delta().insert({ embed_table: this.domNode.innerHTML });
+    return d;
+  }
+
+  // format(name, value) {
+  //   console.log('table format');
+  //   // super.format(name, value);
+  // }
+
+  // formatAt(index, length, name, value) {
+  //   console.log('table formatAt');
+  //   // super.formatAt(index, length, name, value);
+  // }
+
+  // insertAt(index, value, def) {
+  //   console.log('table insertAt');
+  //   super.insertAt(index, value, def);
+  // }
+
+  // optimize() {
+  //   console.log('table optimize');
+  //   super.optimize();
+  // }
+
+  // replace(target) {
+  //   console.log('table replace');
+  //   super.replace(target);
+  // }
+
+  remove() {
+    console.log('table remove');
+    var remove_table = confirm('Are you sure you want to remove this table?');
+    if (remove_table) super.remove();
+  }
+}
+
+EmbedTable.blotName = 'embed_table';
+EmbedTable.tagName = 'table';
+Quill.register(EmbedTable);
 
 //
 //
